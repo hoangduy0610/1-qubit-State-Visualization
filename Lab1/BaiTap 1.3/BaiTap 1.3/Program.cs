6d2b6a35692312575f00b4c3b29be92c0f133e80
TDChinh <135324462+TDChinh@users.noreply.github.com>
2023-10-08 09:42:10 +0700

    Bài tập 1.3 và 1.4

---
+++ Lab1/BaiTap 1.3/BaiTap 1.3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] integers = { 5, 2, 9, 1, 7 };
+        Console.WriteLine("So lon nhat trong day so nguyen: " + FindMax(integers));
+        Console.WriteLine("So nho nhat trong day so nguyen: " + FindMin(integers));
+
+        double[] doubles = { 3.14, 1.618, 2.718, 0.577 };
+        Console.WriteLine("So lon nhat trong day so thuc: " + FindMax(doubles));
+        Console.WriteLine("So nho nhat trong day so thuc: " + FindMin(doubles));
+
+        string[] strings = { "apple", "banana", "cherry", "date" };
+        Console.WriteLine("Chuoi dai nhat trong day chuoi: " + FindMaxLength(strings));
+        Console.WriteLine("Chuoi ngan nhat trong day chuoi: " + FindMinLength(strings));
+    }
+
+    static T FindMax<T>(IEnumerable<T> collection) where T : IComparable<T>
+    {
+        if (collection.Any())
+        {
+            return collection.Max();
+        }
+        throw new InvalidOperationException("Day rong");
+    }
+
+    static T FindMin<T>(IEnumerable<T> collection) where T : IComparable<T>
+    {
+        if (collection.Any())
+        {
+            return collection.Min();
+        }
+        throw new InvalidOperationException("Day rong");
+    }
+
+    static string FindMaxLength(IEnumerable<string> strings)
+    {
+        if (strings.Any())
+        {
+            return strings.OrderByDescending(s => s.Length).First();
+        }
+        throw new InvalidOperationException("Day rong");
+    }
+
+    static string FindMinLength(IEnumerable<string> strings)
+    {
+        if (strings.Any())
+        {
+            return strings.OrderBy(s => s.Length).First();
+        }
+        throw new InvalidOperationException("Day rong");
+    }
+}
---
+++ Lab1/BaiTap 1.4/BaiTap 1.4/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Thermometer
+{
+    private double temperature;
+
+    public event EventHandler TemperatureChanged;
+
+    public double Temperature
+    {
+        get { return temperature; }
+        set
+        {
+            if (value != temperature)
+            {
+                temperature = value;
+                OnTemperatureChanged();
+            }
+        }
+    }
+
+    protected virtual void OnTemperatureChanged()
+    {
+        TemperatureChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
+
+class TemperatureDisplay
+{
+    public void Subscribe(Thermometer thermometer)
+    {
+        thermometer.TemperatureChanged += HandleTemperatureChanged;
+    }
+
+    public void HandleTemperatureChanged(object sender, EventArgs e)
+    {
+        if (sender is Thermometer thermometer)
+        {
+            Console.WriteLine("New temperature: " + thermometer.Temperature);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thermometer thermometer = new Thermometer();
+        TemperatureDisplay display = new TemperatureDisplay();
+
+        display.Subscribe(thermometer);
+
+        Random random = new Random();
+        for (int i = 0; i < 5; i++)
+        {
+            double newTemperature = random.NextDouble() * 100;
+            thermometer.Temperature = newTemperature;
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
