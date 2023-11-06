aca6b0ca68a7a66ecfd77b08b62222d522cc9057
Nguyen Ngoc Minh Quoc <73976668+gundamdouble00@users.noreply.github.com>
2023-11-06 08:36:14 +0700

    Bài 2.4 Lab2

--- Lab2/Exercise_2_4/Form1.cs
+++ Lab2/Exercise_2_4/Form1.cs
@@ -7,39 +7,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Generic;
 
 namespace Exercise_2_4
 {
     public partial class Form1 : Form
     {
         private Random random = new Random();
-
+        private List<string> paintEvents = new List<string>();
         public Form1()
         {
             this.Text = "Paint Event Demo";
             this.Paint += new PaintEventHandler(Form_Paint);
+            this.MouseClick += new MouseEventHandler(Form_MouseClick_Click);
         }
         private void Form_Paint(object sender, PaintEventArgs e)
         {
             // Lấy kích thước của Form
-            int formWidth = this.ClientSize.Width;
-            int formHeight = this.ClientSize.Height;
-
-            // Tạo một Graphics để vẽ trên Form
             Graphics graphics = e.Graphics;
-
-            // Chọn màu và font cho chuỗi vẽ
             Brush textBrush = new SolidBrush(Color.Black);
             Font textFont = new Font("Arial", 12);
 
-            // Tạo vị trí ngẫu nhiên
-            int x = random.Next(0, formWidth);
-            int y = random.Next(0, formHeight);
+            foreach (var paintEvent in paintEvents)
+            {
+                int x = random.Next(0, this.ClientSize.Width);
+                int y = random.Next(0, this.ClientSize.Height);
+                graphics.DrawString(paintEvent, textFont, textBrush, x, y);
+            }
 
-            // Vẽ chuỗi "Paint Event" tại vị trí ngẫu nhiên
-            graphics.DrawString("Paint Event", textFont, textBrush, x, y);
-
-            // Giải phóng các đối tượng Graphics và Brush
             textFont.Dispose();
             textBrush.Dispose();
             graphics.Dispose();
@@ -48,10 +43,20 @@ namespace Exercise_2_4
         //{
         //    InitializeComponent();
         //}
+        //private void Form_MouseClick(object sender, MouseEventArgs e)
+        //{
+            
+        //}
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void Form_MouseClick_Click(object sender, EventArgs e)
+        {
+            paintEvents.Add("Paint Event");
+            this.Invalidate(); // Kích hoạt lại sự kiện Paint để vẽ lại "Paint Event"
         }
     }
 }
