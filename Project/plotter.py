c81cef9fd3ea3cb8d30a7f45508642a0fbda59cc
Duy Nguyen Hoang <hoangduy06104@gmail.com>
2023-12-11 23:51:00 +0900

    complex number

--- Project/plotter.py
+++ Project/plotter.py
@@ -12,7 +12,7 @@ def display_states(states):
     fig = plt.figure()
     fig.canvas.manager.set_window_title('1-qubit states')
     ax = fig.add_subplot(111, projection='3d')
-    quantum_states = [np.array([float(x) for x in line.split()]) for line in states]
+    quantum_states = [np.array([complex(x) for x in line.split()]) for line in states]
     for state in quantum_states:
         theta, phi = calculate_phi_and_theta(state[0], state[1])
         bloch_vector = [np.sin(theta) * np.cos(phi), np.sin(theta) * np.sin(phi), np.cos(theta)]
